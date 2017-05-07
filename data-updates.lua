28f5e3b0c7c76020d895db062b63324c55b2e4b6
Pascal Ludwig <pascal.ludwig@mantro.net>
2017-05-07 13:34:08 +0200

    Initial commit

---
+++ data-updates.lua
@@ -0,0 +1,5 @@
+local lfb = data.raw["item"]["liquid-fuel-barrel"]
+lfb.fuel_category = "chemical"
+lfb.fuel_value = "562.5MJ"
+lfb.fuel_acceleration_multiplier = 1.5
+lfb.fuel_top_speed_multiplier = 1.1
